724a9bbe326ed8922fbd6915c8a98d881ea7cf10
Nilesh9492 <nilesh94041@gmail.com>
2021-11-14 10:13:31 +0530

    Connect to database

---
+++ AddressBookADO/AddressBookRepo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace AddressBookADO
+{
+    class AddressBookRepo
+    {
+        public static string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=AddressBook;Integrated Security=True;";
+        SqlConnection sqlConnection = new SqlConnection(connectionString);
+    }
+}

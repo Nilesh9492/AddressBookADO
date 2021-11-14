4196181698a0a7e5acc7b1d5ac55f6e561949489
Nilesh9492 <nilesh94041@gmail.com>
2021-11-14 17:07:41 +0530

    Create Address Book Table and Retrieve Data

--- AddressBookADO/AddressBookRepo.cs
+++ AddressBookADO/AddressBookRepo.cs
@@ -9,5 +9,47 @@ namespace AddressBookADO
     {
         public static string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=AddressBook;Integrated Security=True;";
         SqlConnection sqlConnection = new SqlConnection(connectionString);
+        public void ViewPersonDetails(SqlDataReader sqlDataReader)
+        {
+
+            AddressBookModel addressBook = new AddressBookModel();
+            addressBook.firstName = Convert.ToString(sqlDataReader["FirstName"]);
+            addressBook.lastName = Convert.ToString(sqlDataReader["LastName"]);
+            addressBook.address = Convert.ToString(sqlDataReader["Address"]);
+            addressBook.city = Convert.ToString(sqlDataReader["City"]);
+            addressBook.stateName = Convert.ToString(sqlDataReader["StateName"]);
+            addressBook.zipCode = Convert.ToString(sqlDataReader["ZipCode"]);
+            addressBook.phonenum = Convert.ToDouble(sqlDataReader["Phonenum"]);
+            addressBook.emailId = Convert.ToString(sqlDataReader["EmailId"]);
+            addressBook.addrBookName = Convert.ToString(sqlDataReader["AddressBookName"]);
+            addressBook.relationType = Convert.ToString(sqlDataReader["RelationType"]);
+            Console.WriteLine("FirstName :{0} LastName :{1} Address :{2} City :{3} State :{4} ZipCode :{5} PhoneNum :{6} EmailId :{7} AddressBookName :{8} RelationType :{9} ", addressBook.firstName, addressBook.lastName, addressBook.address, addressBook.city, addressBook.stateName, addressBook.zipCode, addressBook.phonenum, addressBook.emailId, addressBook.addrBookName, addressBook.relationType);
+            Console.WriteLine("\n");
+        }
+        public int RetrieveData()
+        {
+
+            AddressBookModel model = new AddressBookModel();
+
+            int count = 0;
+            using (sqlConnection)
+            {
+                string query = @"select * from Address_Book_Table";
+                SqlCommand command = new SqlCommand(query, this.sqlConnection);
+                sqlConnection.Open();
+                SqlDataReader sqlDataReader = command.ExecuteReader();
+                if (sqlDataReader.HasRows)
+                {
+                    while (sqlDataReader.Read())
+                    {
+                        ViewPersonDetails(sqlDataReader);
+                        count++;
+                    }
+
+                }
+                sqlDataReader.Close();
+            }
+            return count;
+        }
     }
 }
--- AddressBookADO/Program.cs
+++ AddressBookADO/Program.cs
@@ -7,6 +7,8 @@ namespace AddressBookADO
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World in Address Book ADO!");
+            AddressBookRepo addressBookRepo = new AddressBookRepo();
+            addressBookRepo.RetrieveData();
         }
     }
 }
